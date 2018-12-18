391c7fa0f4f0cfa8176cec9ce85cbc4ccd7286f4
vp-toys-Joker <vgpavlov@mail.ru>
2018-12-18 11:46:09 +0400

    незначительные изменения

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-//    MainWindow w;
-//    w.show();
     RobotControlDlg d;
     QObject::connect(&d, SIGNAL(accepted()), &app, SLOT(quit()));
     d.show();
